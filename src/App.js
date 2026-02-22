f5f08f1dcc5fd877a589804c571b71269cab35ef
s230251 <s230251@student.dtu.dk>
2026-02-22 20:06:58 +0100

    fix build

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './HomePage';
 import DemosPage from './DemosPage';
-import CVPage from './CVPage';
 
 function App() {
   return (
